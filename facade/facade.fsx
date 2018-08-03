1488f71d80de3d0304072ca7108ec1975f8af907
tuominen <tuomin24@student.tut.fi>
2018-08-03 03:47:47 +0300

    Formatted the Facade description

--- facade/facade.fsx
+++ facade/facade.fsx
@@ -6,29 +6,14 @@
 /// 
 
 ///
-/// Applicativity
-///
-/// Use the Facade pattern when
-/// - you want to provide a simple interface to a complex subsystem. Subsystems often get more
-///   complex as they evolve. Most patterns, when applied, result in more and smaller classes. This
-///   makes the subsystem more reusable and easier to customize, but it also becomes harder to use
-///   for clients that don't need to customize it. A facade can provide a simple default view of
-///   the subsystem that is good enough for most clients. Only clients needing more customizability
-///   will need to look beyond the facade.
-/// - there are many dependencies between clients and the implementation classes of an abstraction.
-///   Introduce a facade to decouple the subsystem from clients and other subsystems, thereby
-///   promoting subsystem independence and portability.
-/// - you want to layer your subsystems. Use a facade to define an entry point to each subsystem
-///   level. If subsystems are dependent, then you can simplify the dependencies between them by
-///   making them communicate with each other solely through their facades.
+/// CONCLUSION
 /// 
-
-///
-/// REMARKS
+/// In functional programming natural way to implement Facade is to use function composition. This
+/// way a low level or otherwise difficult to use interface may be turned to more abstract and easy
+/// to understand interface.
 /// 
-/// Seems that Facade design pattern is usable also in the functional programming. It is very
-/// natural in functional programming to compose multiple functions simpler functions to one
-/// more complext function. This is exactly how the Facade works in the functional programming.
+/// If only one single function is converted behind the Facade, then the parttern is similar with
+/// functional Adapter.
 /// 
 
 ///

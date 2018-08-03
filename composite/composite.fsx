c7ebe9dec62975d1fdc3907549196123693ff55f
tuominen <tuomin24@student.tut.fi>
2018-08-03 03:11:40 +0300

    Reimplemented Composite

--- composite/composite.fsx
+++ composite/composite.fsx
@@ -1,157 +1,57 @@
-//
-// COMPOSITE
-//
-// Compose objects into tree structures to represent part-whole hierarchies. Composite lets clients
-// treat individual objects and compositions of objects uniformly.
-//
-
-//
-// NOTES
-//
-// Type structure of Composite is implemented with discriminated union in functional programming.
-// Functionality is set of functions operating with the discriminated union type. Traversing
-// through the composite is done by recursion.
-//
-// Recursion function can be parameterized to take arbitrary functions to perform different 
-// operations to the items 
-//
-// More information from Real-World Functional Programming page 200
-//
-//
+///
+/// COMPOSITE
+///
+/// Compose objects into tree structures to represent part-whole hierarchies. Composite lets clients
+/// treat individual objects and compositions of objects uniformly.
+///
 
 ///
-/// REMOVING COMPONENT
+/// CONCLUSION
 /// 
-/// Removing of a component requires an identification to be added to all components. In object-
-/// oriented world all objects have a unique id since pointer (memory address) usage. This is not
-/// supported in functional world.
+/// In functional programming natural way to implement composite is to use discriminated union. If
+/// at least one of the types is a container for the discriminated union type, then it is a
+/// composite.
 /// 
-/// It also leads to some confusing situations to allow removel of components. For example
-/// removal the root element from the graph results to what? 
+/// Functions taking the discriminated union types as parameters operate the samy way from the
+/// client point of view for single and composite values. Usually these functions use recursion to
+/// traverse through the values containing composites.
+/// 
+/// Visitor pattern may be used to create a generic way to traverse composite structures and apply
+/// functions to contained values.
 /// 
 
+///
+/// EXAMPLE
+/// 
+/// In this simple example a discriminated union is used to present a shape. A shap may be a single
+/// circle, single square or composite (aka any combination of those).
+/// 
+/// Client code may use any of the functions supporting the shape type without knowing if the shape
+/// is a single item or a more complex composition.
+/// 
 
-//=================================================================================================
-// Example - Begin
-//=================================================================================================
+// Simple discriminated union type to present complex shapes
+type Shape = Circle of int | Square of int | Composite of Shape list
 
-module Example1 =
-    // Define the type hierarchy
-    type Graphic = Picture of Picture | Line of Line | Rectangle of Rectangle | Text of Text
-    and Picture = { Parts : Graphic list }
-    and Line = { From : (int * int); To : (int * int) }
-    and Rectangle = { TopLeft : (int * int); BottomRight : (int * int) }
-    and Text = { String : string; Position : (int * int) }
-
-
-    // Define the functionality of the types
-
-    ///
-    /// Adds a new part to an existing graphic
-    /// 
-    let add part graphic =
-        match graphic with
-            | Picture(picture) ->
-                printfn "appending to picture"
-                Graphic.Picture { Parts = List.append picture.Parts [part] }
-            | Line(line) ->
-                printfn "appending to line"
-                Graphic.Picture { Parts = [ Graphic.Line line; part ] }
-            | Rectangle(rectangle) ->
-                printfn "appending to rectangle"
-                Graphic.Picture { Parts = [ Graphic.Rectangle rectangle; part ] }
-            | Text(text) ->
-                printfn "appending to text"
-                Graphic.Picture { Parts = [ Graphic.Text text; part ] }
-
-    ///
-    /// 
-    /// 
-    // let rec remove graphic part =
-    //     match graphic with
-    //         | Picture(picture) ->
-    //             picture.Parts |> 
+// A function to calculate size of the shape
+let rec area shape =
+    match shape with
+        | Circle radius ->
+            let pi = 3
+            radius * radius * pi
+        | Square size ->
+            size * size
+        | Composite parts ->
+            parts |> List.fold (fun state shape -> state + area shape) 0
 
 
+let test() =
+    let square : Shape = Square 2
+    let circle : Shape = Circle 10
+    let composite : Shape = Composite [ Square 2; Square 4 ]
 
-    ///
-    /// Counts how many components the graphic contains
-    /// 
-    let rec componentCount (graphic : Graphic) : int =
-        match graphic with
-            | Picture(picture)
-                -> 1 + (picture.Parts |> Seq.sumBy componentCount)
-            | _ -> 1
+    printfn "size of %A is %A" square (area square)
+    printfn "size of %A is %A" circle (area circle)
+    printfn "size of %A is %A" composite (area composite)
 
-    let line : Graphic = Line { From = (10, 10); To = (20, 10) }
-    let rect : Graphic = Rectangle { TopLeft = (10, 10); BottomRight = (20, 10) }
-
-    let pict : Graphic = Picture { Parts = [ line; rect] }
-
-
-
-    line
-    |> add line
-    |> add rect
-    |> add pict
-
-//=================================================================================================
-// Example - End
-//=================================================================================================
-
-module Example2 =
-
-    type Composite = Leaf of Leaf | Node of Node
-    and Leaf = string
-    and Node = Composite list
-
-    let draw composite =
-        match composite with
-            | Composite.Leaf(leaf) -> printf " %s " leaf
-            | Composite.Node(_) -> ()
-
-
-    let rec forAll func composite =
-        match composite with
-            | Leaf(_) ->
-                func composite
-            | Node(node) -> 
-                node |> List.iter (fun t ->
-                    forAll func t)
-
-    let add part composite =
-        match composite with 
-            | Node(node) ->
-                Composite.Node (List.append node [part])
-            | Leaf(_) ->
-                Composite.Node (List.append [composite] [part])
-
-
-open Example2
-
-let brown = Composite.Leaf "a brown"
-let fox = Composite.Leaf "fox"
-let jumped = Composite.Leaf "jumped"
-let over = Composite.Leaf "over"
-let sleepy = Composite.Leaf "a sleepy"
-let dog = Composite.Leaf "dog"
-
-let first = add fox brown
-let second = add over jumped
-let third =  add dog sleepy
-
-let root =
-    Composite.Node []
-    |> add first
-    |> add second
-    |> add third
-
-
-forAll draw root
-
-forAll draw brown
-
-let draw = forAll Example2.draw
-
-draw brown
-draw root
+test()

10402d981720e5e22a7e7ea972bc74d2071f8058
tuominen <tuomin24@student.tut.fi>
2018-08-05 20:05:43 +0300

    Improved the Flyweight to use memoization

--- flyweight/flyweight.fsx
+++ flyweight/flyweight.fsx
@@ -23,90 +23,102 @@
 ///
 
 ///
-/// Example
+/// EXAMPLE
 /// 
-/// In this example a scenery is created from rocks and trees. Most of the rocks and trees are the
-/// same, so they can be presented by the same data item. Multiple trees can be combined to create
-/// a forest.
+/// This example demonstrates the impact to memory usage and speed when flyweight is used. Program
+/// generates a forest of 100 trees. A forest is modeled with three different tree types - oaks,
+/// maples and beeches - which are pesudo-randomly positioned. The constant data utilized by the 
+/// flyweight is the tree, which always have the same 10 000 specific leaves. The variating data is
+/// the pseudo-random position of an individual tree.
 /// 
-/// Example demonstrates how using the flyweight pattern in F# reduces the memory usage. The benefit
-/// is greater on bigger flyweights.
-/// 
 
-// General definitions
-type Shape = Circle | Square | Triangle
-type Color = Green | Blue | Gray
+module LazyList =
+    type List<'a> = Empty | Node of Lazy<'a> * Lazy<List<'a>>
+    let item index ls =
+        let rec indexer n ls =
+            match ls with
+                | Node (a, ls) ->
+                    if n = 0
+                    then a.Force()
+                    else indexer (n - 1) (ls.Force())
+        indexer index ls
 
-// External data of flyweights (not shared)
-type Placement = { X : int; Y : int }
-
-// Inner data of different flyweights (shared among)
-type Rock = { Shape : Shape; Color : Color; Size : int }
-type Tree = { Shape : Shape; Color : Color; }
-type Forest = (Tree * Placement) list
-
-// SceneryItem can be a tree, a rock or a forest (corresbonds to base class for the structure)
-type SceneryItem = Tree of Tree | Rock of Rock | Forest of Forest
-
-// Pre-determined tree and rock types
-type TreeType = Oak | Fir | Mapple
-type RockType = Small | Big
-
-let treeFactory treeType =
-    let oak = { Shape = Circle; Color = Green }
-    let fir = { Shape = Triangle; Color = Green }
-    let mapple = { Shape = Square; Color = Green }
-
-    match treeType with
-    | Oak -> oak
-    | Fir -> fir
-    | Mapple -> mapple
-
-let rockFactory rockType =
-    let small = { Shape = Circle; Color = Gray; Size = 3 }
-    let big = { Shape = Square; Color = Gray; Size = 10 }
-
-    match rockType with
-    | Small -> small
-    | Big -> big
+    let rec initInfinite (f : int -> 'a) : List<'a> =
+        let rec initializer index f =
+            let a = lazy (f index)
+            let ls = lazy (initializer (index + 1) f)
+            Node (a, ls)
+        initializer 0 f
 
 
-let flyweightClient() = 
-    System.GC.Collect()
-    let memoryBefore = System.GC.GetTotalMemory(true)
+module Flyweight =
+    let memoize (f : int -> 'a) : (int -> 'a) =
+        let inifinityList = (LazyList.initInfinite f)
+        fun i -> (inifinityList |> LazyList.item i)
+    
 
-    let forest1 = List.init 10000000 (fun _ -> treeFactory Fir)
-    let forest2 = List.init 10000000 (fun i -> if i % 2 = 0 then treeFactory Oak else treeFactory Mapple)
+module Framework =
+    // Some leaf types to create different trees
+    type Leaf = Oak | Maple | Beech
+    
+    // Factory to create various trees
+    let treeFactory key =
+        let oak = List.replicate 10000 Leaf.Oak
+        let maple = List.replicate 10000 Leaf.Maple
+        let beech = List.replicate 10000 Leaf.Beech
 
-    System.GC.Collect()
-    let memoryAfter = System.GC.GetTotalMemory(true)
+        [oak; maple; beech] |> List.item (key % 3)
 
-    printfn "forest 1 size=%A and forest 2 size=%A" forest1.Length forest2.Length
+    // Pseudo-random number generator (not essential for the example)
+    let rec prng count modulus a c seed =
+        match count with
+            | 0 ->
+                []
+            | _ ->
+                let seed = (a * seed + c) % modulus
+                seed::(prng (count - 1) modulus a c seed)
+        
+    // Client creating a forest
+    let clientTemplate memoize =
+        let treeCount = 100
 
-    memoryAfter - memoryBefore
+        System.GC.Collect()
+        let memoryBefore = System.GC.GetTotalMemory(true)
 
+        // Flyweights are used or not depending of the memoize implementation
+        let memo = memoize treeFactory
 
-let regularClient() = 
-    System.GC.Collect()
-    let memoryBefore = System.GC.GetTotalMemory(true)
+        // Constant data is the actual tree (Oak, Maple or Beech)
+        let trees = List.init treeCount (fun i -> memo (i % 10))
 
-    let forest1 = List.init 10000000 (fun _ -> { Shape = Triangle; Color = Green })
-    let forest2 = List.init 10000000 (fun i ->
-        if i % 2 = 0 then { Shape = Circle; Color = Green } else { Shape = Square; Color = Green })
+        // Variating data is the position of the tree
+        let xPositions = prng treeCount (1 <<< 8) 1664525 1013904223 11
+        let yPositionss = prng treeCount (1 <<< 8) 1664525 1013904223 17
 
-    System.GC.Collect()
-    let memoryAfter = System.GC.GetTotalMemory(true)
+        let forest = List.zip3 trees xPositions yPositionss
 
-    printfn "forest 1 size=%A and forest 2 size=%A" forest1.Length forest2.Length
+        System.GC.Collect()
+        let memoryAfter = System.GC.GetTotalMemory(true)
 
-    memoryAfter - memoryBefore
+        printfn "forest size=%A" forest.Length
 
+        ((memoryAfter - memoryBefore), forest)
 
 let test() =
-    let flyweightMemoryUsage = flyweightClient() / (1024L * 1024L)
-    let regularMemoryUsage = regularClient() / (1024L * 1024L)
+    // Flyweight client uses memoize pattern to reduce runtime and memory usage
+    let flyweightClient() = Framework.clientTemplate Flyweight.memoize
 
-    printfn "flyweight memory usage: %A MB" flyweightMemoryUsage
-    printfn "regular memory usage: %A MB" regularMemoryUsage
+    // Regular client uses treeFactory directly without caching
+    let regularClient() = Framework.clientTemplate id
+
+    let toMegaBytes bytes = bytes / 1024L / 1024L
+
+    let (regularMemory, regularForest) = regularClient()
+    let (flyweightMemory, flyweightForest) = regularClient()
+
+    printfn "reglar memory concumption %A MB" (regularMemory |> toMegaBytes)
+    printfn "flyweight memory concumption %A MB" (flyweightMemory |> toMegaBytes)
+    printfn "identical forests = %A" (regularForest = flyweightForest)
+    flyweightForest
 
 test()
--- flyweight/memoize.fsx
+++ flyweight/memoize.fsx
@@ -1,7 +1,7 @@
-type LazyList<'T> = Empty | Node of Lazy<'T> * Lazy<LazyList<'T>>
+module LazyList =
+    type Data<'T> = Empty | Node of Lazy<'T> * Lazy<Data<'T>>
 
-type LazyList<'T> with
-    static member Item index ls =
+    let item index ls =
         let rec indexer n ls =
             match ls with
                 | Node (a, ls) ->
@@ -10,55 +10,31 @@ type LazyList<'T> with
                     else indexer (n - 1) (ls.Force())
         indexer index ls
 
-let rec initInfinite (index : int) (f : int -> 'a) : LazyList<'a> =
-    let a = lazy (f index)
-    let ls = lazy (initInfinite (index + 1) f)
-    Node (a, ls)
+    let rec initInfinite (index : int) (f : int -> 'a) : Data<'a> =
+        let a = lazy (f index)
+        let ls = lazy (initInfinite (index + 1) f)
+        Node (a, ls)
     
-
-
-let memoize (f : int -> 'a) : (int -> 'a) =
-    let inifinityList = (initInfinite 0 f)
-    fun i -> (inifinityList |> LazyList<'a>.Item i)
-
-// let memo = memoize float
-
-// memo 2000001
-
-
-
-
-
-// let infinity = initInfinite 0 float
-
-// infinity |> LazyList<float>.Item 1000000
-
-
-// let lazyList = Node (lazy 10, lazy Node (lazy (List.sum [0.0 .. 50000000.0] |> int), lazy Node (lazy 30, lazy Empty)))
-
-// lazyList |> LazyList<int>.Item 0
-
-
-
-
-
-
-
-
+module Memoize =
+    let memoize (f : int -> 'a) : (int -> 'a) =
+        let inifinityList = (LazyList.initInfinite 0 f)
+        fun i -> (inifinityList |> LazyList.item i)
 
 let treeFactory key =
+    let trees = List.init 10 (fun i -> List.replicate 10000 i)
+    trees |> List.item key
     // let treeOne = List.replicate 1000 0
     // let treeTwo = List.replicate 1000 1
 
     // if key = 0 then treeOne else treeTwo
-    List.replicate 10000 key
+    //List.replicate 10000 key
 
 let clientTemplate memoize =
     System.GC.Collect()
     let memoryBefore = System.GC.GetTotalMemory(true)
 
     let memo = memoize treeFactory
-    let forest = List.init 1000 (fun i -> memo (i % 2))
+    let forest = List.init 1000 (fun i -> memo (i % 10))
 
     System.GC.Collect()
     let memoryAfter = System.GC.GetTotalMemory(true)
@@ -67,7 +43,7 @@ let clientTemplate memoize =
 
     memoryAfter - memoryBefore
 
-let flyweightClient() = clientTemplate memoize
+let flyweightClient() = clientTemplate Memoize.memoize
 let regularClient() = clientTemplate id
 
 
@@ -84,3 +60,7 @@ let rec prng count modulus a c seed =
             seed::(prng (count - 1) modulus a c seed)
 
 prng 10 (1 <<< 8) 1664525 1013904223 5
+
+
+//let ls = LazyList.Node (5, LazyList.LazyList<int>.Empty)
+let ls = LazyList.Data<int>.Node (lazy(5), lazy(LazyList.Data<int>.Empty))

46f1fe838f5bbd16c7678de158f84e1ae8b5fe5f
functional-patterns <41644822+functional-patterns@users.noreply.github.com>
2018-08-13 11:26:39 +0300

    Update interpreter.hs

--- interpreter/interpreter.hs
+++ interpreter/interpreter.hs
@@ -1,26 +1,24 @@
 
 data Expression = Literal Literal | Operation Operation deriving (Show, Eq)
-data Literal = One | Two | Three deriving (Show, Eq)
+type Literal = Int
 data Operation = Plus Expression Expression | Minus Expression Expression deriving (Show, Eq)
 
-evaluateExpression :: Expression -> Int
+evaluateExpression :: Expression -> Literal
 evaluateExpression (Literal l) = evaluateLiteral l
 evaluateExpression (Operation o) = evaluateOperation o
 
-evaluateLiteral :: Literal -> Int
-evaluateLiteral One = 1
-evaluateLiteral Two = 2
-evaluateLiteral Three = 3
+evaluateLiteral :: Literal -> Literal
+evaluateLiteral = id
 
-evaluateOperation :: Operation -> Int
+evaluateOperation :: Operation -> Literal
 evaluateOperation (Plus a b) = (evaluateExpression a) + (evaluateExpression b)
 evaluateOperation (Minus a b) = (evaluateExpression a) - (evaluateExpression b)
 
 main :: IO ()
 main = do
-    let one = Literal One
-    let two = Literal Two
-    let three = Literal Three
+    let one = Literal 1
+    let two = Literal 2
+    let three = Literal 3
 
     let sum = Operation $ Plus two three
     let expression = Operation $ Minus sum one
